915c059e3692fa80fcd6d34f25b81bf4533381b4
chinese name : 钱晟龙 english name: Arc Qian <286999915@qq.com>
2016-09-02 15:42:18 +0800

    增加jvm各项指标监控, @Timed , @Counted , @Gauge , @ExceptionMetered , @CachedGauge , @Metric , 所有的例子s.

--- src/main/java/dropwizard/metrics/config/SpringMetricsConfig.java
+++ src/main/java/dropwizard/metrics/config/SpringMetricsConfig.java
@@ -1,9 +1,16 @@
 package dropwizard.metrics.config;
 
+import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.jvm.FileDescriptorRatioGauge;
+import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
+import com.codahale.metrics.jvm.ThreadStatesGaugeSet;
 import com.ryantenney.metrics.spring.config.annotation.EnableMetrics;
 import com.ryantenney.metrics.spring.config.annotation.MetricsConfigurerAdapter;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Arc on 29/8/2016.
@@ -16,10 +23,16 @@ public class SpringMetricsConfig extends MetricsConfigurerAdapter {
     public void configureReporters(MetricRegistry metricRegistry) {
         // registerReporter allows the MetricsConfigurerAdapter to
         // shut down the reporter when the Spring context is closed
-//        registerReporter(ConsoleReporter
-//                .forRegistry(metricRegistry)
-//                .build())
-//                .start(5, TimeUnit.SECONDS);
+        registerReporter(ConsoleReporter
+                .forRegistry(metricRegistry)
+                .build())
+                .start(5, TimeUnit.SECONDS);
+
+        metricRegistry.register("jvm.gc", new GarbageCollectorMetricSet());
+        metricRegistry.register("jvm.memory", new MemoryUsageGaugeSet());
+        metricRegistry.register("jvm.thread-states", new ThreadStatesGaugeSet());
+        metricRegistry.register("jvm.fd.usage", new FileDescriptorRatioGauge());
+
     }
 
 }
--- src/main/java/dropwizard/metrics/controller/HelloWorldController.java
+++ src/main/java/dropwizard/metrics/controller/HelloWorldController.java
@@ -1,10 +1,14 @@
 package dropwizard.metrics.controller;
 
-import com.codahale.metrics.annotation.Gauge;
-import com.codahale.metrics.annotation.Timed;
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.UniformReservoir;
+import com.codahale.metrics.annotation.*;
 import dropwizard.metrics.bean.Person;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Arc on 17/8/2016.
@@ -12,20 +16,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("say")
 public class HelloWorldController {
+
     @Gauge
     private long helloTime;
 
     @RequestMapping("hello")
-    @Timed
     public String sayHello() {
         helloTime ++;
         return "Hello world!";
     }
 
-    @RequestMapping("person")
+    @Metric
+    public Histogram histogram = new Histogram(new UniformReservoir());
+
     @Timed
+    @RequestMapping("person")
     public Person getPerson() {
         return new Person().setName("shenglong").setPassword("wobugaosuni");
     }
+
+    @Counted
+    @RequestMapping("count")
+    public Person count() {
+        return new Person().setName("countExample").setPassword("I like you.");
+    }
+
+    @ExceptionMetered
+    @RequestMapping("exception-meter")
+    public Person exception() {
+        throw new NullPointerException("test");
+        //return new Person().setName("exceptionExample").setPassword("I admire you.");
+    }
+
+    Random random = new Random();
+    @CachedGauge(name = "queueSize", timeout = 30, timeoutUnit = TimeUnit.SECONDS)
+    public int getQueueSize() {
+        //return queue.getSize();
+        return random.nextInt();
+    }
+
 
 }
